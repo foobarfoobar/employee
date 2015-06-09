f03277ee696dfed3401ba0cf45a0b9f4d49a8ebd
Foo Bar <crowley.13@gmx.de>
2015-06-09 14:24:24 +0200

    validation (email, name) styling error messages

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,11 @@
 class Employee < ActiveRecord::Base
   belongs_to :department # ein Employee gehoert zu einem Department
+  
+  validates :lastname, presence: true
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: VALID_EMAIL_REGEX, uniqueness: true, presence: true
+  
   # Konstante, single_point_of_trust, dry
   # hier muss I18n.t statt t genutzt werden!!
   TITLES = [I18n.t("text.woman"), I18n.t("text.woman_doc"), I18n.t("text.man"), I18n.t("text.man_doc")]
