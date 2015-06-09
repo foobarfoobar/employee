2de74d2a4e5792eae5285ad7e96dc8f53ecde00a
Foo Bar <crowley.13@gmx.de>
2015-06-09 11:10:52 +0200

    added constant added Anrede-field added column to table, migration

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ActiveRecord::Base
+  # Konstante, single_point_of_trust, dry
+  # hier muss I18n.t statt t genutzt werden!!
+  TITLES = [I18n.t("text.woman"), I18n.t("text.woman_doc"), I18n.t("text.man"), I18n.t("text.man_doc")]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608132545) do
+ActiveRecord::Schema.define(version: 20150609085457) do
 
   create_table "employees", force: true do |t|
     t.string   "firstname"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150608132545) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "freelancer"
+    t.string   "title"
   end
 
 end
