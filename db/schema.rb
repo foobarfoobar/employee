df46ca5daa8f728b4b9748096ca34b9bf4621db2
Foo Bar <crowley.13@gmx.de>
2015-06-09 11:23:45 +0200

    forgotten migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609085457) do
+ActiveRecord::Schema.define(version: 20150609091304) do
+
+  create_table "departments", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "employees", force: true do |t|
     t.string   "firstname"
